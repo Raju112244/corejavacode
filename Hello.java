5b81cba7a5e23bf7486d5b78a05a3eca39bc67fd
Raju112244 <78521053+Raju112244@users.noreply.github.com>
2021-12-21 13:35:50 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+	public static void main(String[] args) 
+	{
+		
+       System.out.println("Hello world");
+       System.out.println("    *   ");
+       System.out.println("   **   ");
+       System.out.println("  *** ");
+       System.out.println(" hello rajuuuu ");
+	}
+
+}
